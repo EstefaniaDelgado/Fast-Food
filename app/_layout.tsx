20e57f03b3230e6989d6163ea308a665a02042c3
EstefaniaDelgado <teff_64@hotmail.com>
2025-08-12 10:11:41 -0500

    feature(bottonNavigation):add navigation with icons

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,14 @@
+// app/_layout.js
 import { Stack } from 'expo-router';
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      {/* Pantalla inicial sin tabs */}
+      <Stack.Screen name="index" />
+
+      {/* Grupo de pantallas que sí tendrán tabs */}
+      <Stack.Screen name="home" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
